47040a0d70f49eab1c526a52230c54da4a4319a1
rui <349763010@qq.com>
2018-09-10 23:53:46 +0800

    update schedule

---
+++ src/main/java/Company/OPPO/Main.java
@@ -0,0 +1,12 @@
+package Company.OPPO;
+
+import java.util.Scanner;
+
+/**
+ * Created by rui on 2018/9/10.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
